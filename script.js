e99b6c31ccb3712325a1389cc85539b45c5ebf02
litanha <141042264+litanha@users.noreply.github.com>
2025-03-11 09:45:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao= document.getElementById
+    ('aumentar-fonte');
+    const diminuirFonteBotao= document.getElementById
+    ('diminuir-fonte');
+
+    let tamanhoAtualFonte=1;
+
+    aumentaFonteBotao.addEventListener('click',function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${ tamanhoAtualFonte}rem`
+       })
+        diminuirFonteBotao.addEventListener('click',function(){
+            tamanhoAtualFonte -= 0.1;
+            document.body.style.fontSize= `${tamanhoAtualFonte}rem`
+        })
+
+        const botaoDeAcessibilidade= document.getElementById('botao-acessibilidade');
+        const opcoesDeAcessibilidade= document.getElementById('opcoes-acessibilidade');
+        botaoDeAcessibilidade.addEventListener('click' , function(){
+            botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+            opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+        })
+ })
